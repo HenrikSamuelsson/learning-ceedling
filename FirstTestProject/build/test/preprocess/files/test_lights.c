101e49ceffd7be412a11545c1069f21c434902c2
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2020-04-08 13:06:10 +0200

    Results of creating a module called lights.

---
+++ FirstTestProject/build/test/preprocess/files/test_lights.c
@@ -0,0 +1,30 @@
+#include "build/temp/_test_lights.c"
+#include "src/lights.h"
+#include "C:/Ruby26-x64/lib/ruby/gems/2.6.0/gems/ceedling-0.29.1/vendor/unity/src/unity.h"
+
+
+
+
+void setUp(void)
+
+{
+
+}
+
+
+
+void tearDown(void)
+
+{
+
+}
+
+
+
+void test_lights_NeedToImplement(void)
+
+{
+
+    UnityIgnore( (("Need to Implement lights")), (UNITY_UINT)(15));
+
+}
---
+++ FirstTestProject/build/test/preprocess/includes/test_lights.c
@@ -0,0 +1,4 @@
+---
+- C:/Ruby26-x64/lib/ruby/gems/2.6.0/gems/ceedling-0.29.1/vendor/unity/src/unity.h
+- src/lights.h
+- build/temp/_test_lights.c
