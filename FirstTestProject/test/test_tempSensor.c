ff7b1192c306b106837dc731ec9c605e7bef2ff0
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2020-04-09 11:46:44 +0200

    Add a test for the temperature sensor.

---
+++ FirstTestProject/test/test_tempSensor.c
@@ -0,0 +1,31 @@
+#include "tempSensor.h"
+#include "unity.h"
+
+
+void setUp(void)
+{
+    /* Do nothing. */
+}
+
+
+void tearDown(void)
+{
+    /* Do nothing. */
+}
+
+
+void test_whenTempRegisterReadsMaxValue_thenTheTempIsTheMaxValue(void)
+{
+    const uint8_t TEMP_REGISTER_ADDRESS = 0x03;
+    float expectedTemp = 104.6f;
+    float tolerance = 0.1f;
+    
+    /* When the temperature register of mock temperature sensor holds 0x3ff. */
+    i2c_readRegister_ExpectAndReturn(TEMP_REGISTER_ADDRESS, 0x3ff);
+    
+    /* Then we expect to to get a certain temperature */
+    float actualTemperature = tempSensor_getTemperature();
+
+    /* Test that we got the expected temperature. */
+    TEST_ASSERT_FLOAT_WITHIN(tolerance, expectedTemp, actualTemperature);
+}
